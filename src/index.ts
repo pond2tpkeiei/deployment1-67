39423ac02719af062bae30b5fd152ad61e05edaa
Nungnon <Pisit_ch@cmu.ac.th>
2024-10-09 18:33:16 +0700

    TestTest

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
+
 export function BMI(weight: number, height: number): string {
-    return (weight / (height * height)).toFixed(2);
-  }
-  
-  const weight: number = 60;
-  const height: number = 1.75;
-  console.log(`Your weight: ${weight} kg.\nYour height: ${height} m.\nYour BMI: ${BMI(weight, height)}`);
-  
+  if (height <= 0) throw new Error("Height must be greater than 0");
+  return (weight / (height * height)).toFixed(2);
+}
+
+const weight: number = 60;
+const height: number = 1.75;
+console.log(`Your weight: ${weight} kg.\nYour height: ${height} m.\nYour BMI: ${BMI(weight, height)}`);
--- src/test.ts
+++ src/test.ts
@@ -6,3 +6,7 @@ const height = 1.75;
 const bmi = (weight / (height * height)).toFixed(2);
 
 assert.strictEqual(BMI(weight, height), bmi, `Expected : ${bmi}\nResult : ${BMI(weight, height)}`);
+
+assert.throws(() => BMI(weight, 0), /Height must be greater than 0/, "Height must be greater than 0 test failed");
+
+console.log('All tests passed!');
