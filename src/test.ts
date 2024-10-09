15efaa45fd39ec42912667062dcc74b921f9035a
Pattarapon <151112816+pond2tpkeiei@users.noreply.github.com>
2024-10-09 21:43:12 +0700

    ||

--- src/test.ts
+++ src/test.ts
@@ -1,7 +1,7 @@
 import { BMI } from './index';
 import * as assert from 'assert';
 
-const weight = 87;
+const weight = 85;
 const height = 180;
 const bmi = (weight / (height * height)).toFixed(2);
 
