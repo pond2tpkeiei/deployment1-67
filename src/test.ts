06e470b73f49f6f335f02c282f73362dc36b12c4
Taowawawawawawawawawawawawawa <151112961+Taowawawawawawawawawawawawawa@users.noreply.github.com>
2024-10-09 17:01:18 +0700

    edit bmi in test case toFixed(2)

--- src/test.ts
+++ src/test.ts
@@ -3,6 +3,6 @@ import * as assert from 'assert';
 
 const weight = 60;
 const height = 1.75;
-const bmi = (weight / (height * height));
+const bmi = (weight / (height * height)).toFixed(2);
 
 assert.strictEqual(BMI(weight, height), bmi, `Expected : ${bmi}\nResult : ${BMI(weight, height)}`);
