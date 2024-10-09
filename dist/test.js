14d2d29a2c212f582c2fb15ab3435d5f96e70ea6
Pattarapon <151112816+pond2tpkeiei@users.noreply.github.com>
2024-10-09 22:34:57 +0700

    |||||||

--- dist/test.js
+++ dist/test.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var index_1 = require("../src/index");
 var assert = require("assert");
-var weight = 85;
+var weight = 83;
 var height = 1.80;
 var bmi = (weight / (height * height)).toFixed(2);
 assert.strictEqual((0, index_1.BMI)(weight, height), bmi, "Expected : ".concat(bmi, "\nResult : ").concat((0, index_1.BMI)(weight, height)));
